8d71ab94ad97e6bd0e724debb7c40150c3432395
npl22 <nli22@jhu.edu>
2017-05-10 14:20:45 -0700

    Add pokemon reducer, store

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.store = store;
   ReactDOM.render(<h1>It works!</h1>, root);
 });
+
+
+// testing
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+window.fetchAllPokemon = fetchAllPokemon;
+window.receiveAllPokemon = receiveAllPokemon;
---
+++ pokedex/frontend/util/api_util.js
@@ -0,0 +1,3 @@
+export const fetchAllPokemon = () => (
+  $.ajax({url: "api/pokemon"})
+);
